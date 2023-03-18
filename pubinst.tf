c75006c158ef5477c3a6cc178713f54883df5bf7
dinesh <dinesh@gmail.com>
2023-03-18 21:46:01 +0530

    first commit

---
+++ pubinst.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "public1" {
+  ami                         = var.amipub
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  user_data                   = file("user1.sh")
+
+  tags = {
+    Name = "PUB-INSTANCE-1"
+  }
+}
+resource "aws_instance" "public2" {
+  ami                         = var.amipub
+  instance_type               = var.instance_type  
+  associate_public_ip_address = true
+  user_data                   = file("user1.sh")
+
+  tags = {
+    Name = "PUB-INSTANCE-2"
+  }
+}
---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "amipub" {
+  type        = string
+  default     = "ami-05afd67c4a44cc983"
+  description = "description"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "description"
+}
